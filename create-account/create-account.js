ce49c30e8d64f6764f03328c31d1293e4091580a
Karl Sigel <heykarl@gmail.com>
2021-09-08 15:36:10 -0700

    added event listener to sign-up page

---
+++ create-account/create-account.js
@@ -0,0 +1,27 @@
+import { setUser } from '../local-storage-utils.js';
+
+const inputForm = document.querySelector('#input-form');
+
+inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(inputForm);
+
+    const newUser = data.get('make-name');
+
+    const password = data.get('mk-pwd');
+  
+    const userData = {
+        name: newUser,
+        password: password,
+        todos: [
+            {
+                id: '',
+                todo: '',
+                completed:{}
+            }
+        ]
+    };
+    setUser(userData);
+    window.location = '../todo-list-page';
+});
--- local-storage-utils.js
+++ local-storage-utils.js
@@ -1,14 +1,14 @@
 
 const USERDATA = 'USERDATA';
 
+export function setUser(userData) {
+    const stringyArray = JSON.stringify(userData);
+    localStorage.setItem(USERDATA, stringyArray);
+}
+
 export function getUser() {
     const stringyUser = localStorage.getItem(USERDATA);
     return JSON.parse(stringyUser);
-}
-
-export function setUser() {
-    const stringyArray = JSON.stringify(userData);
-    localStorage.setItem(USERDATA, stringyArray);
 }
 
 export function getTodos() {
@@ -16,7 +16,7 @@ export function getTodos() {
 }
 
 export function setTodos() {
-
+    const user = getUser();
 }
 
 export function addTodo() {
